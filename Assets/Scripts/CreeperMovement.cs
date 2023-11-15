fd0b471946b96e3367e7b6b9bbb29644090832a7
Bausch, Jan <jan.bausch@7p-group.com>
2023-11-15 11:17:46 +0100

    Add creeper physics and sun

---
+++ Assets/Scripts/CreeperMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreeperMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed = 2.5f;
+    public float jumpForce = 5.0f;
+    public float detectionRadius = 5.0f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3();
+
+        moveDirection.y -= 9.81f * Time.deltaTime;
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpForce * Time.deltaTime;
+            }
+        }
+
+        // Check if player is close
+        if ((target.transform.position - transform.position).magnitude < detectionRadius)
+        {
+            // Target player
+            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+
+            // Move forward
+            moveDirection += transform.forward * Time.deltaTime * moveSpeed;
+        }
+
+        controller.Move(moveDirection);
+    }
+}
