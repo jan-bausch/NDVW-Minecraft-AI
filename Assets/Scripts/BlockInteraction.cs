4e4114c4527abde20f4efdc5a7ea40b981bc7232
Jensdboc <jensdb@telenet.be>
2023-11-21 17:13:32 +0100

    Round to correct coordinate

--- Assets/Scripts/BlockInteraction.cs
+++ Assets/Scripts/BlockInteraction.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class BlockInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Camera")]
+    public Camera playerCamera;
+    public float rayTime = 1.0f;
+    public float range = 5.0f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Assuming left mouse button for breaking
         {
-            Ray ray = new Ray(transform.position, Vector3.down);
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, rayTime);
+
+            if (Physics.Raycast(ray, out hit, range))
             {
                 Vector3 hitPointOnMesh = hit.point;
 
-                // Round the coordinates to integers
-                int roundedX = Mathf.RoundToInt(hitPointOnMesh.x);
-                int roundedY = Mathf.RoundToInt(hitPointOnMesh.y);
-                int roundedZ = Mathf.RoundToInt(hitPointOnMesh.z);
+                // Round the coordinates tFloorToInto integers
+                int roundedX = Mathf.(hitPointOnMesh.x);
+                int roundedY = Mathf.FloorToInt(hitPointOnMesh.y);
+                int roundedZ = Mathf.FloorToInt(hitPointOnMesh.z);
 
                 Vector3 roundedHitPoint = new Vector3(roundedX, roundedY, roundedZ);
 
